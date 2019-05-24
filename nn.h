eac8d51563f36e6f5c4f0bf01cf62fbc3b83d612
Adrien Soursou <26015179+ChuOkupai@users.noreply.github.com>
2019-05-25 00:57:23 +0200

    basic perceptron

--- nn.h
+++ nn.h
@@ -1,6 +1,13 @@
 #ifndef _NN_H
 # define _NN_H
 
+struct perceptron;
+typedef struct perceptron perceptron_t;
 
+perceptron_t*	perceptron_create(int size);
+
+int	perceptron_learn(perceptron_t *p, int *sample, int expected);
+
+void	perceptron_destroy(perceptron_t *p);
 
 #endif
