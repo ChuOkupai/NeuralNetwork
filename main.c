ffedb339a033b5ae9c3a6b2b22c4541f1bf408ea
Adrien Soursou <26015179+ChuOkupai@users.noreply.github.com>
2019-05-28 14:00:50 +0200

    update

--- main.c
+++ main.c
@@ -15,21 +15,22 @@ int	main()
 {
 	perceptron_t *p = perceptron_create(5);
 	// taille 5 pour stocker les 4 pixels du motif + le neurone de sortie
-	int t[4], sample, expected, valid = 0;
+	int session = 0, t[4], sample, expected, valid = 0;
 	printf("Detection of sample 1001:\n");
 	while (valid < 16) // 16 motifs au total possibles
 	{
-		sample = 15;
+		session++;
+		sample = 0;
 		valid = 0;
-		printf("new learning session\n");
-		while (sample >= 0)
+		printf("Learning session %d:\n", session);
+		while (sample < 16)
 		{
 			expected = (sample == 9) ? 1 : 0;
 			export(t, sample);
 			printf("sample: %d%d%d%d\n", t[0], t[1], t[2], t[3]);
 			if (perceptron_learn(p, t, expected) == expected)
 				valid++;
-			sample--;
+			sample++;
 		}
 	}
 	printf("learning completed!\n");
